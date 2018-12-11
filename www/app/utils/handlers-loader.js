88f34cd234be132598daa69f47db55cb12d014fb
Skitsanos <1391820+skitsanos@users.noreply.github.com>
2018-12-11 17:24:50 +0200

    Update handlers-loader.js
    modified for using promises

--- www/app/utils/handlers-loader.js
+++ www/app/utils/handlers-loader.js
@@ -1,7 +1,7 @@
 /**
  * Express JS handlers loader
  * @author skitsanos
- * @version 2.14.02
+ * @version 2.20181211.02
  */
 
 const fs = require('fs');
@@ -10,54 +10,62 @@ const handlers = require('../request-handlers/map').handlers;
 module.exports = {
     path: global.appRoot + '/app/request-handlers/',
 
-    load: function (container, callback)
+    load: function (container)
     {
         let _root = this.path;
 
-        for (let h = 0; h < handlers.length; h++)
+        return new Promise((resolve, reject) =>
         {
-            let endpoint = handlers[h];
-            if (endpoint.method === undefined)
-                endpoint.method = 'get';
-
-            if (endpoint.handler === undefined)
+            if (!fs.existsSync(_root))
             {
-                global.log.error(`Handler is not defined for ${endpoint.path} (${endpoint.method })`);
+                return reject({message: _root + ' not found'});
             }
-            else
+
+            for (let h = 0; h < handlers.length; h++)
             {
-                let jsFile = _root + endpoint.handler;
+                let endpoint = handlers[h];
+                if (endpoint.method === undefined)
+                    endpoint.method = 'get';
 
-                if (fs.existsSync(jsFile + '.js'))
+                if (endpoint.handler === undefined)
                 {
-                    global.log.info(`loading handler for ${endpoint.path} ${endpoint.method !== undefined ? '(' + endpoint.method.toUpperCase() + ')' : ''}`);
-
-                    switch (endpoint.method.toLowerCase())
-                    {
-                        case 'get':
-                            container.get(endpoint.path, require(jsFile));
-                            break;
-
-                        case 'put':
-                            container.put(endpoint.path, require(jsFile));
-                            break;
-
-                        case 'post':
-                            container.post(endpoint.path, require(jsFile));
-                            break;
-
-                        case 'delete':
-                            container.delete(endpoint.path, require(jsFile));
-                            break;
-                    }
+                    global.log.error(`Handler is not defined for ${endpoint.path} (${endpoint.method })`);
                 }
                 else
                 {
-                    global.log.error(`Handler not loaded for ${endpoint.path} ${endpoint.method !== undefined ? '(' + endpoint.method.toUpperCase() + ')' : ''}`);
+                    let jsFile = _root + endpoint.handler;
+
+                    if (fs.existsSync(jsFile + '.js'))
+                    {
+                        global.log.info(`loading handler for ${endpoint.path} ${endpoint.method !== undefined ? '(' + endpoint.method.toUpperCase() + ')' : ''}`);
+
+                        switch (endpoint.method.toLowerCase())
+                        {
+                            case 'get':
+                                container.get(endpoint.path, require(jsFile));
+                                break;
+
+                            case 'put':
+                                container.put(endpoint.path, require(jsFile));
+                                break;
+
+                            case 'post':
+                                container.post(endpoint.path, require(jsFile));
+                                break;
+
+                            case 'delete':
+                                container.delete(endpoint.path, require(jsFile));
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        global.log.error(`Handler not loaded for ${endpoint.path} ${endpoint.method !== undefined ? '(' + endpoint.method.toUpperCase() + ')' : ''}`);
+                    }
                 }
             }
-        }
 
-        callback();
+            resolve(true);
+        });
     }
 };
